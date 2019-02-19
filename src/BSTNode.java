a1eab38b1e6db6f38077136e3b7a02e6c8a207a1
unknown <vpratha@vt.edu>
2019-02-18 19:01:09 -0500

    configured build path

--- src/BSTNode.java
+++ src/BSTNode.java
@@ -1,4 +1,50 @@
 
 public class BSTNode<T> {
-
+    
+    private T element;
+    private BSTNode<T> leftNode;
+    private BSTNode<T> rightNode;
+    
+    public BSTNode(T elem)
+    {
+        element = elem;
+        leftNode = null;
+        rightNode = null;
+    }
+    
+    public T getElement()
+    {
+        return element;
+    }
+    
+    public BSTNode<T> getLeftNode()
+    {
+        return leftNode;
+    }
+    
+    public BSTNode<T> getRightNode()
+    {
+        return rightNode;
+    }
+    
+    public void setElement(T elem)
+    {
+        element = elem;
+    }
+    
+    public void setLeftNode(BSTNode<T> left)
+    {
+        leftNode = left;
+    }
+    
+    public void setRightNode(BSTNode<T> right)
+    {
+        rightNode = right;
+    }
+    
+    public String toString()
+    {
+        return element.toString();
+    }
+    
 }
