a070b6b3f73b1c152631d2dacaf2b8ae3cac7aaf
unknown <vpratha@vt.edu>
2019-02-23 17:31:38 -0500

    added bst test

---
+++ src/BSTTest.java
@@ -0,0 +1,56 @@
+import student.TestCase;
+
+public class BSTTest {
+    
+    private BST<Rectangle> bst;
+    
+    /**
+     *  Sets up test cases.
+     */
+    public void setUp() 
+    {
+        bst = new BST<Rectangle>();
+    }
+
+    /**
+     *  Tests insert()
+     */
+    public void testGetRootNode() 
+    {
+
+    }
+
+    /**
+     *  Tests insert()
+     */
+    public void testGetIterator() 
+    {
+
+    }
+    
+    /**
+     *  Tests insert()
+     */
+    public void testInsert() 
+    {
+
+    }
+    
+    /**
+     *  Tests remove()
+     */
+    public void testRemove() 
+    {     
+  
+    }
+
+    /**
+     *  Tests findMin(), findMax()
+     */
+    public void testDump() 
+    {
+
+    }
+
+
+}
