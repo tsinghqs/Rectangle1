1d4f931ebe5b1d3ce30a9fc602b8d8de9b5b4ad5
tsinghqs <tsingh2324@gmail.com>
2019-02-23 19:25:04 -0500

    Update iterator tests

--- src/IteratorTest.java
+++ src/IteratorTest.java
@@ -9,6 +9,8 @@ public class IteratorTest<T> extends student.TestCase {
     private Iterator<Integer> tester;
     private BSTNode<Integer> rootNode = new BSTNode<Integer>(5);
     private BSTNode<Integer> left = new BSTNode<Integer>(2);
+    private BSTNode<Integer> right = new BSTNode<Integer>(5);
+    private Iterator<Integer> tester1;
 
     
     /**
@@ -17,14 +19,18 @@ public class IteratorTest<T> extends student.TestCase {
     public void setUp() 
     {
         tester = new Iterator<Integer>(rootNode);
+        tester1 = new Iterator<Integer>(null);
+
     }
     
     public void testHasNext()
     {
         assertTrue(tester.hasNext());
         rootNode.setLeftNode(left);
+        rootNode.setRightNode(right);
         assertEquals(rootNode, tester.next());
-        
+        tester.next();
+        assertFalse(tester1.hasNext());
     }
     
     
